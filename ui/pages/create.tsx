f193c4d6d118d66e29d7345fa8b01f251e56f23d
Steve Smodish <smodish@hotmail.com>
2021-03-29 20:55:14 -0400

    Create Poll form progress, add timer functionality
    Co-authored-by: digipa <digipa@users.noreply.github.com>
    Co-authored-by: Allan Tito <allantito@users.noreply.github.com>
    Co-authored-by: maxinemull <maxinemull@users.noreply.github.com>
    Co-authored-by: Gabe Eipper <Gabester0@users.noreply.github.com>
    Co-authored-by: Miguel Gomez <gomem45@outlook.com>

--- ui/pages/create.tsx
+++ ui/pages/create.tsx
@@ -1,14 +1,72 @@
-import { FormControl, FormLabel, Container, Input } from '@chakra-ui/react'
+import { useState } from 'react'
+import {
+  Heading,
+  Stack,
+  IconButton,
+  FormControl,
+  FormLabel,
+  Container,
+  Input,
+  Button,
+  ButtonGroup,
+  Box,
+} from '@chakra-ui/react'
+import { AddIcon, MinusIcon } from '@chakra-ui/icons'
 
 export default function Create() {
+  const [timerDays, setTimerDays] = useState(0)
+  const [timerHours, setTimerHours] = useState(0)
+  const [timerMinutes, setTimerMinutes] = useState(0)
+  const [timerSeconds, setTimerSeconds] = useState(0)
+
+  const [timerLength, setTimerLength] = useState(0)
+
+  // Handle timer
+
   return (
     <>
       <Container>
         <FormControl id='what'>
-          <FormLabel>What is this about?</FormLabel>
+          <FormLabel>What is this poll about?</FormLabel>
           <Input type='text' />
         </FormControl>
-        // Set deadline // Add options // Get link to share
+        <FormLabel>Set timer for poll</FormLabel>
+        <Stack spacing={4} direction='row' align='center'>
+          <ButtonGroup size='md' isAttached>
+            <Button>1 Day</Button>
+            <IconButton aria-label='Add days' icon={<AddIcon />} />
+            <IconButton aria-label='Subtract days' icon={<MinusIcon />} />
+          </ButtonGroup>
+          <ButtonGroup size='md' isAttached>
+            <Button>1 Hour</Button>
+            <IconButton aria-label='Add hours' icon={<AddIcon />} />
+            <IconButton aria-label='Subtract hours' icon={<MinusIcon />} />
+          </ButtonGroup>
+          <ButtonGroup size='md' isAttached>
+            <Button>1 Minute</Button>
+            <IconButton aria-label='Add minutes' icon={<AddIcon />} />
+            <IconButton aria-label='Subtract minutes' icon={<MinusIcon />} />
+          </ButtonGroup>
+          <ButtonGroup size='md' isAttached>
+            <Button>1 Second</Button>
+            <IconButton aria-label='Add seconds' icon={<AddIcon />} />
+            <IconButton aria-label='Subtract seconds' icon={<MinusIcon />} />
+          </ButtonGroup>
+        </Stack>
+        <Box bg='green'>{timerLength}</Box>
+        <FormControl id='choices'>
+          {/* How do we add choices dynamically? */}
+
+          <FormLabel>What are the choices?</FormLabel>
+          <Input type='text' />
+          <Input type='text' />
+          <Input type='text' />
+          <Input type='text' />
+        </FormControl>
+
+        <Button colorScheme='red'>Submit</Button>
+
+        {/* Get link to share */}
       </Container>
     </>
   )
