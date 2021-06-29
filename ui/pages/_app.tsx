c3b166528da315a1aa551f3b108c2f6728f92ac8
flauge2u <valiant1.christie@gmail.com>
2021-06-28 21:02:58 -0400

    removed layout

--- ui/pages/_app.tsx
+++ ui/pages/_app.tsx
@@ -2,33 +2,23 @@ import '../styles/global.scss'
 
 import { ChakraProvider } from '@chakra-ui/react'
 import { AppProps } from 'next/app'
-import AppContext, { IAppContext } from '../state/AppContext'
-import { useState } from 'react'
-import Layout from '../components/Layout/layout'
+import AppContext from '../state/AppContext'
+// import AppContext, { IAppContext } from '../state/AppContext'
+// import { useState } from 'react'
+// import Layout from '../components/Layout/layout'
 import { PollWrapper } from '../context/polls'
 
 function App({ Component, pageProps }: AppProps) {
-  const [openedMenu, setOpenedMenu] = useState<boolean>(true)
-  const toggleMenu: () => void = () => setOpenedMenu(!openedMenu)
-  const [currentRoute, setCurrentRoute] = useState<string>('')
-  const getCurrentRoute = (route: string) => setCurrentRoute(route)
-
-  const initialState: IAppContext = {
-    openedMenu: openedMenu,
-    toggleMenu: toggleMenu,
-    currentRoute: currentRoute,
-    getCurrentRoute: getCurrentRoute,
-  }
-
+  
   return (
     <ChakraProvider>
-      <AppContext.Provider value={initialState}>
+      {/* <AppContext.Provider value={initialState}> */}
         <PollWrapper>
-        <Layout>
+        
           <Component {...pageProps} />
-        </Layout>
+        
         </PollWrapper>
-      </AppContext.Provider>
+      {/* </AppContext.Provider> */}
     </ChakraProvider>
   )
 }
